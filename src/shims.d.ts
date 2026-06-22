97660b1322001dfad7d03946dcd0d3fac23a38ac
kingebrahimopq-create <kingebrahimopq@gmail.com>
2026-06-22 12:19:51 +0300

    fix: src/shims.d.ts

--- src/shims.d.ts
+++ src/shims.d.ts
@@ -1,46 +1,12 @@
-declare module '*.svg' {
-  const content: string;
-  export default content;
+declare module '*.json' {
+  const value: any;
+  export default value;
 }
 
-declare module '*.png' {
-  const content: string;
-  export default content;
+interface Window {
+  electronAPI?: {
+    googleSignIn: () => Promise<{ success: boolean; name?: string; email?: string; avatar?: string; accessToken?: string; error?: string }>;
+    listDriveBackups: () => Promise<any[]>;
+    downloadFromDrive: (id: string) => Promise<any>;
+  };
 }
-
-declare module '*.jpg' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.jpeg' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.gif' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.ico' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.woff2' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.woff' {
-  const content: string;
-  export default content;
-}
-
-declare module '*.ttf' {
-  const content: string;
-  export default content;
-}
-
-export {};
